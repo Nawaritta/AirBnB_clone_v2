ebaa5e11d29bdf6a13b05227b53fe8fb369eedd3
hassan <hassan.hbmama@gmail.com>
2023-09-20 12:19:19 -0700

    tasks8

--- models/user.py
+++ models/user.py
@@ -4,6 +4,8 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -14,4 +16,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user", cascade="delete")
 
